0741430fa2859d5fa9295c3996f4b1d669e15d08
Nehanshu Parikh <142480139+NehanshuParikh@users.noreply.github.com>
2023-12-11 16:05:01 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/instagramClone");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  name: String,
+  email: String,
+  profileImage: String,
+  bio: String,
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post"
+  }]
+})
+
+userSchema.plugin(plm);
+module.exports = mongoose.model("user", userSchema);
